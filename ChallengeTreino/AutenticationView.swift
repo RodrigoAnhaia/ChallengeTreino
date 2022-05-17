7d6af448cd0f15511865ac35cc28d4ca341f9e57
RodrigoS.A <rodrigoanhaia2@hotmail.com>
2022-05-17 20:28:22 -0300

    First Commit
    added .gitignore.

---
+++ ChallengeTreino/AutenticationView.swift
@@ -0,0 +1,20 @@
+//
+//  AutenticationView.swift
+//  ChallengeTreino
+//
+//  Created by Rodrigo de Anhaia on 17/05/22.
+//
+
+import SwiftUI
+
+struct AutenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AutenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AutenticationView()
+    }
+}
---
+++ ChallengeTreino/ViewModel/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  ChallengeTreino
+//
+//  Created by Rodrigo de Anhaia on 17/05/22.
+//
+
+import Foundation
+
+
+extension ContentView {
+    @MainActor class ViewModel: ObservableObject {
+        
+    }
+}

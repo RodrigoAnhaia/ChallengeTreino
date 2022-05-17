10a82e628413b72a710206fb6da36cccc814bf84
RodrigoS.A <rodrigoanhaia2@hotmail.com>
2022-05-17 09:47:12 -0300

    Initial Commit

---
+++ ChallengeTreino/ChallengeTreinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTreinoApp.swift
+//  ChallengeTreino
+//
+//  Created by Rodrigo de Anhaia on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTreinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

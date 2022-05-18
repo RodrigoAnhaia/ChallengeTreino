2cb41f0373a586513d416ca1a7ba2221f77505c2
RodrigoS.A <rodrigoanhaia2@hotmail.com>
2022-05-18 20:58:58 -0300

    Part of CRUD
    added some variables and functions for CRUD.

---
+++ ChallengeTreino/Controllers/PersistenceController.swift
@@ -0,0 +1,41 @@
+//
+//  PersistenceController.swift
+//  ChallengeTreino
+//
+//  Created by Rodrigo de Anhaia on 18/05/22.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolve error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "ChallengeTreino")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
